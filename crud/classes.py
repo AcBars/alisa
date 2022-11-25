2015400b7483ab385ae839bf9ec672a47cc6ca96
Artem_notebook <mobail_2003@bk.ru>
2022-11-25 15:44:46 +0700

    Добавил архитектуру проекта

--- crud/classes.py
+++ crud/classes.py
@@ -7,3 +7,25 @@ def insert_classes(level, symbol):
     cursor.execute(''' INSERT INTO classes (level, symbol) VALUES(?, ?) ''',
                    (level, symbol))
     conn.commit()
+    
+    
+def delete_classes():
+    try:
+        conn = sqlite3.connect('sqlite_python.db')
+        cursor = conn.cursor()
+        print("Подключен к SQLite")
+
+        sql_delete_query = """DELETE from classes where id = 6"""
+        cursor.execute(sql_delete_query)
+        conn.commit()
+        print("Запись успешно удалена")
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        if conn:
+            conn.close()
+            print("Соединение с SQLite закрыто")
+
+delete_record()
